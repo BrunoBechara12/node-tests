38577ee06ffd96d936e37852071985fa4e761383
Bruno Bechara <Brunosbechara@gmail.com>
2024-11-10 16:42:30 -0300

    feat: create new test verifying start date

--- src/entities/appointment.test.ts
+++ src/entities/appointment.test.ts
@@ -5,7 +5,8 @@ test("create an appointment", () => {
   const startsAt = new Date();
   const endsAt = new Date();
 
-  endsAt.setDate(endsAt.getDate() + 1);
+  endsAt.setDate(endsAt.getDate() + 3);
+  startsAt.setDate(startsAt.getDate() + 1);
 
   const appointment = new Appointment({
     customer: "Bruno Bechara",
@@ -21,11 +22,28 @@ test("cannot create an appointment with end date earlier than start date", () =>
   const startsAt = new Date();
   const endsAt = new Date();
 
-  endsAt.setDate(endsAt.getDate() - 1);
+  endsAt.setDate(endsAt.getDate());
+  startsAt.setDate(startsAt.getDate() + 1);
 
   expect(() => {
     return new Appointment({
       customer: "Bruno Bechara",
+      startsAt,
+      endsAt,
+    });
+  }).toThrow();
+});
+
+test("cannot create an appointment with start date earlier than now", () => {
+  const startsAt = new Date();
+  const endsAt = new Date();
+
+  endsAt.setDate(endsAt.getDate() + 5);
+  startsAt.setDate(startsAt.getDate() - 1);
+
+  expect(() => {
+    return new Appointment({
+      customer: "Bruno",
       startsAt,
       endsAt,
     });
