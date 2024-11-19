f92d39b2e650ed4e07e5256648931d48b632cda2
Bruno Bechara <Brunosbechara@gmail.com>
2024-11-18 21:00:35 -0300

    fix: adjustment in the getFutureDate test

--- src/tests/utils/get-futute-date.test.ts
+++ src/tests/utils/get-futute-date.test.ts
@@ -4,5 +4,5 @@ import { getFutureDate } from "./get-future-date";
 test("increases date with one year", () => {
   const year = new Date().getFullYear();
 
-  expect(getFutureDate(`${year}-10-10`).getFullYear()).toEqual(2025);
+  expect(getFutureDate(`${year}-10-10`).getFullYear()).toEqual(year + 1);
 });
